1c5ef08d29d54b064599a61ee60cd8e8084a27da
Tuukka Wahtera <tuukka.wahtera@futurice.com>
2018-04-06 21:08:00 +0300

    TowardsTarget

--- wheelie.ino
+++ wheelie.ino
@@ -1,4 +1,5 @@
 #include <SPI.h>
+#include <math.h>
 
 #define MOSI 23
 #define MISO 19
@@ -12,10 +13,16 @@
 #define ADR_VOLATILE     0b00000000;
 #define ADR_NON_VOLATILE 0b00100000;
 
+#define ACCELERATION 5
+
+uint32_t current, target;
+
 void setWiper(uint32_t cs, uint32_t value) {
   // the write command is two bytes wide
   // first 7 bytes for address and command,
   // 9 for the value in range 0...256.
+  Serial.print("Value: "); Serial.print(value); Serial.print("\n");
+  
   uint8_t byte1 = 0x00, byte2 = 0x00;
 
   byte1 = CMD_WRITE;
@@ -32,17 +39,37 @@ void setWiper(uint32_t cs, uint32_t value) {
   digitalWrite(cs, HIGH);
 }
 
+uint32_t towardsTarget(uint32_t cur, uint32_t targ) {
+  int32_t dist = targ-cur;
+  int32_t sig = abs(dist)/dist;
+
+  /* Serial.print("sig: ");Serial.print(sig);Serial.print("\n"); */
+  /* Serial.print("target: ");Serial.print(target);Serial.print("\n"); */
+  
+  if(abs(dist) > ACCELERATION) {
+    return cur + sig*ACCELERATION;
+  }
+  return targ;
+}
 
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(SS, OUTPUT);
   // initialize SPI:
   SPI.begin(SCK, MISO,MOSI, SS);
   SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+
+  current = 0; target = 0;
 }
 
 void loop() {
-  delay(3000);
-  setWiper(SS, 255);
-  delay(3000);
-  setWiper(SS, 0);
+  if(current == 0) {target = 256;}
+  if(current == 256) {target = 0;}
+
+  uint32_t next = towardsTarget(current, target);
+  setWiper(SS, next);
+
+  current = next;
+  delay(100);
 }
